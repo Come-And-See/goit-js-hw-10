aff43fe9f94f8878c4482c26f52d5222307395f4
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-05-02 17:25:18 +0300

    UP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import './css/styles.css';
+import API from './js/fetchCountries';
+import { Notify } from 'notiflix';
+
+import countrysFunction from './templates/countrys.hbs';
+import countrysFunction2 from './templates/countrysFull.hbs';
+
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onTEST, DEBOUNCE_DELAY));
+
+function onTEST(e) {
+  if (e.target.value.trim().length === 0) {
+    onClearHTML();
+    return;
+  }
+
+  API.fetchCountries(`${e.target.value.trim()}`).then(country => {
+    if (country.length > 10) {
+      onAlert();
+      return;
+    }
+    if (country.length !== 1) {
+      onCountryAll(country);
+      return;
+    }
+    onCountryOne(country);
+  });
+}
+
+function onAlert() {
+  Notify.info('Too many matches found. Please enter a more specific name.');
+  onClearHTML();
+}
+
+function onCountryAll(country) {
+  const CountryAll = countrysFunction(country);
+  refs.countryInfo.innerHTML = CountryAll;
+}
+
+function onCountryOne(country) {
+  const CountryOne = countrysFunction2(country);
+  refs.countryInfo.innerHTML = CountryOne;
+}
+
+function onClearHTML() {
+  refs.countryInfo.innerHTML = '';
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}`).then(r => {
+    return r.json();
+  })
+}
+
+export default { fetchCountries };
